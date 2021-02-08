70b88fe2da60772b4c6d228aa2b262e331dd2633
jinwoo kim <ko440338@gmail.com>
2021-02-08 17:23:02 +0900

    코드 합침!

--- DDL.sql
+++ DDL.sql
@@ -1,9 +1,11 @@
 create table member(
    member_no number not null ,
-   pw varchar2(20) not null,
+   pw varchar2(100) not null,
    name varchar2(20) not null,
    email varchar2(20) not null,
    regdate date default sysdate,
+   enabled number(1,0) not null,
+   role varchar2(20) not null,
    constraint pk_member primary key(member_no)
 );
 
@@ -19,45 +21,64 @@ CREATE TABLE board(
     constraint pk_board primary key(board_no),
     constraint fk_member foreign key(member_no) references member(member_no) on Delete CASCADE
 );
+CREATE TABLE teamrank (
+    team_no number not null,
+    rank NUMBER(20),
+    game NUMBER(20),
+    win NUMBER(20),
+    draw NUMBER(20),
+    loss NUMBER(20),
+    score NUMBER(20),
+    losspoint NUMBER(20),
+    gapscore NUMBER(20),
+    point NUMBER(20),
+    season VARCHAR2(2000),
+    teamname VARCHAR2(2000),
+    teamimage VARCHAR2(2000),
+    leaguename VARCHAR2(2000),
+    constraint pk_team primary key(team_no)
+);
 
 
 CREATE TABLE player (
     player_no number not null,
     game NUMBER(20) not null,
     score NUMBER(20) not null,
-    playername VARCHAR2(20) not null,
+    playername VARCHAR2(2000) not null,
     assist NUMBER(20) not null,
     attackpoint Number(38) not null,
     shoot NUMBER(38) not null,
-    shotontarget NUMBER(38) not null,
+    shootontarget NUMBER(38) not null,
     foul NUMBER(38) not null,
     yellowcard NUMBER(20) not null,
     redcard NUMBER(20) not null,
     offside NUMBER(20) not null,
-    teamname VARCHAR2(20) not null,
-    playerimage VARCHAR2(100) not null,
-    leaguename VARCHAR2(100) not null,
+    teamname VARCHAR2(2000) not null,
+    playerimage VARCHAR2(2000),
+    leaguename VARCHAR2(2000) not null,
+    rank NUMBER(20) not null,
+    season VARCHAR2(2000) not null,
     constraint pk_player primary key(player_no)
 );
-
 CREATE TABLE gameinformation(
     game_no number not null,
-    leaguename VARCHAR2(100) not null,
-    homename VARCHAR2(100) not null,
-    homescore Number(10) not null,
-    homeimage VARCHAR2(2000) not null,
-    awayname VARCHAR2(100) not null,
-    awayscore Number(10) not null,
-    awayimage VARCHAR2(10) not null,
-    round NUMBER(10) not null,
-    stadium VARCHAR2(10) not null,
-    matchdate date default sysdate,
-    homewin Number(1,0) not null,
-    awaywin Number(1,0) not null,
-    season NUMBER(10) not null,
+    leaguename VARCHAR2(100),
+    homename VARCHAR2(100),
+    homescore VARCHAR2(100),
+    homeimage VARCHAR2(2000),
+    awayname VARCHAR2(100),
+    awayscore VARCHAR2(100),
+    awayimage VARCHAR2(2000),
+    round VARCHAR2(100),
+    stadium VARCHAR2(100),
+    matchdate VARCHAR2(200),
+    homewin VARCHAR2(100),
+    awaywin VARCHAR2(100),
+    season VARCHAR2(100),
+    matchtime VARCHAR2(100),
+    gamestatus VARCHAR2(100), 
     constraint pk_gameinformation primary key(game_no)
 );
-
 CREATE TABLE answer(
     answer_no number not null,
     member_no number not null,
@@ -77,3 +98,4 @@ CREATE TABLE star(
     constraint fk_memberNo foreign key(member_no) references member(member_no) on Delete CASCADE
 );
 
+create sequence seq_teamrank;
